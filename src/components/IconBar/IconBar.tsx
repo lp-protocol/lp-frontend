669e751bb3e35a8a0aefd8e54be4e335f963dda1
0xdeployer <ethnftboi@protonmail.com>
2022-11-01 10:56:21 -0700

    adding link to github

--- src/components/IconBar/IconBar.tsx
+++ src/components/IconBar/IconBar.tsx
@@ -8,8 +8,11 @@ import styles from "./styles.module.scss";
 const data = [
   { href: "https://twitter.com/TheLPxyz", img: twitter },
   { href: "https://discord.gg/GNUHtbyFyB", img: discord },
-  { href: "https://etherscan.io/address/0x38930aae699c4cd99d1d794df9db41111b13092b", img: etherscan },
-  // { href: "", img: github },
+  {
+    href: "https://etherscan.io/address/0x38930aae699c4cd99d1d794df9db41111b13092b",
+    img: etherscan,
+  },
+  { href: "https://github.com/lp-protocol", img: github },
 ];
 
 export function IconBar() {
