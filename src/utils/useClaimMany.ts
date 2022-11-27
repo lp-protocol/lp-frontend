6c3cc1f2fd6d8b6080621a4de24bbcc297c555b7
0xflick <github@0xflick.xyz>
2022-11-26 23:51:22 -0600

    bug: fix crash when no injected provider is present

--- src/utils/useClaimMany.ts
+++ src/utils/useClaimMany.ts
@@ -2,23 +2,33 @@ import React, { useMemo } from "react";
 import { BigNumber } from "ethers";
 import { useContractWrite, usePrepareContractWrite } from "wagmi";
 
-const claimManyAbi = [{
-  "inputs": [
-    {
-      "internalType": "uint256[]",
-      "name": "nftIds",
-      "type": "uint256[]"
-    }
-  ],
-  "name": "claimMany",
-  "outputs": [],
-  "stateMutability": "nonpayable",
-  "type": "function"
-}] as const;
+const claimManyAbi = [
+  {
+    inputs: [
+      {
+        internalType: "uint256[]",
+        name: "nftIds",
+        type: "uint256[]",
+      },
+    ],
+    name: "claimMany",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
 
-export function useClaimMany(tokenIds: string[], writeConfig?: ReturnType<typeof useContractWrite<typeof claimManyAbi, "claimMany">>) {
-  const tokens = useMemo(() => tokenIds.map((tokenId) => BigNumber.from(tokenId)), [tokenIds]);
-  const { config, isError } = usePrepareContractWrite({
+export function useClaimMany(
+  tokenIds: string[],
+  writeConfig?: ReturnType<
+    typeof useContractWrite<typeof claimManyAbi, "claimMany">
+  >
+) {
+  const tokens = useMemo(
+    () => tokenIds.map((tokenId) => BigNumber.from(tokenId)),
+    [tokenIds]
+  );
+  const { config } = usePrepareContractWrite({
     address: process.env.REACT_APP_LP_CONTRACT,
     abi: claimManyAbi,
     functionName: "claimMany",
@@ -27,10 +37,13 @@ export function useClaimMany(tokenIds: string[], writeConfig?: ReturnType<typeof
   });
 
   const gasLimit = BigNumber.from(config?.request?.gasLimit.toString() ?? "0");
-  config.request.gasLimit = gasLimit.add(gasLimit.div(4));
-  
+
   const writeData = useContractWrite({
     ...config,
+    request: {
+      ...config.request,
+      gasLimit: gasLimit.add(gasLimit.div(4)),
+    },
     ...writeConfig,
   });
 
